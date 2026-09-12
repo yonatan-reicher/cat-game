c23775a8eaed19981309cd6e2a8a67dc1f30d167
Jonathan Reicher <yony252525@gmail.com>
2026-09-12 13:24:36 +0300

    update

--- src/Main.elm
+++ src/Main.elm
@@ -7,11 +7,16 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 import Random
 -- Our imports
+import Localization exposing (..)
 import Game exposing (..)
 
 
 type alias Flags = ()
-type Model
+type alias Model =
+  { local : Local
+  , modelState : ModelState
+  }
+type ModelState
   = Menu
   | Game Game
 type Msg
@@ -30,51 +35,102 @@ main =
 
 
 init : Flags -> ( Model, Cmd Msg )
-init flags = ( Menu, Cmd.none )
+init flags = ( { local = Hebrew, modelState = Menu }, Cmd.none )
 
 
 view : Model -> Document Msg
 view model =
-  { title = "Cat Cafe Game The Game"
+  { title =
+      lStringGet
+        model.local
+        { en = "Cat Cafe Game"
+        , he = "בית קפה חתולים"
+        }
   , body = body model
   }
 
 
 body : Model -> List (Html Msg)
 body model =
-  case model of
-    Menu -> menu
-    Game g -> game g
+  case model.modelState of
+    Menu -> menu model
+    Game g -> game model g
 
 
-menu : List (Html Msg)
-menu =
+menu : Model -> List (Html Msg)
+menu m =
   [ button
       [ onClick NewGame ]
-      [ text "New Game"
+      [ text
+        <| lStringGet m.local
+        <| { en = "New Game", he = "משחק חדש" }
       ]
   ]
 
 
-game : Game -> List (Html Msg)
-game g =
+game : Model -> Game -> List (Html Msg)
+game model g =
   [ div
       [ style "display" "flex"
       , style "flex-direction" "column"
       ]
-      (hand g.hand)
+      (hand model g.hand)
+  , event model (List.head g.events)
   ]
 
 
-hand : Hand -> List (Html Msg)
-hand h = List.map card h
+hand : Model -> Hand -> List (Html Msg)
+hand m h = List.map (card m) h
 
 
-card : Card -> Html Msg
-card c =
+card : Model -> Card -> Html Msg
+card m c =
   case c of
-    Cat -> span [] [ text "Cat" ]
-    ResourceCard CatFood -> span [] [ text "Cat Food" ]
+    Cat ->
+      span
+        []
+        [ text
+          <| lStringGet m.local
+          <| { en = "Kitty", he = "חתולי" }
+        ]
+    ResourceCard CatFood ->
+      span
+        []
+        [ text
+          <| lStringGet m.local
+          <| { en = "Cat Food", he = "אוכל חתולים" }
+        ]
+
+
+event : Model -> Maybe Event -> Html Msg
+event m maybe =
+  div
+    []
+  <| case maybe of
+      Nothing -> []
+      Just e ->
+        [ text <| lStringGet m.local e.name
+        , div
+            [ style "display" "flex"
+            , style "flex-direction" "row"
+            , style "justify-content" "center"
+            ]
+            [ option m e.option1
+            , option m e.option2
+            , option m e.option3
+            ]
+        ]
+
+
+option : Model -> Maybe Option -> Html Msg
+option m maybeO =
+  case maybeO of
+    Nothing -> div [] []
+    Just o ->
+      div
+        []
+        [ text <| lStringGet m.local o.text
+        ]
 
 
 type alias Update = Model -> ( Model, Cmd Msg )
@@ -82,7 +138,14 @@ update : Msg -> Update
 update msg =
   case msg of
     EmptyMsg -> \model -> ( model, Cmd.none )
-    NewGame -> \_ -> ( Game <| newGame <| Random.initialSeed 0, Cmd.none )
+    NewGame -> newGame
+
+
+newGame : Model -> ( Model, Cmd Msg )
+newGame m =
+  ( { m | modelState = Game <| Game.newGame <| Random.initialSeed 0 }
+  , Cmd.none
+  )
 
 
 subscriptions : Model -> Sub Msg
